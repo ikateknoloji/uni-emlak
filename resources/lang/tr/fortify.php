c52b8818db70cb3a398505d1cd11657cf22756db
Kaan Kara <ikateknoloji@gmail.com>
2025-05-12 17:41:05 +0300

    temel dosyalar

---
+++ resources/lang/tr/fortify.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+ 'two_factor_authentication' => [
+  'enabled' => 'İki faktörlü kimlik doğrulama etkinleştirildi.',
+  'disabled' => 'İki faktörlü kimlik doğrulama devre dışı bırakıldı.',
+  'invalid' => 'Sağlanan iki faktörlü kimlik doğrulama kodu geçersiz.',
+ ],
+ 'passwords' => [
+  'reset' => 'Şifreniz başarıyla sıfırlandı!',
+  'sent' => 'Şifre sıfırlama bağlantınız e-posta ile gönderildi!',
+  'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+  'token' => 'Bu şifre sıfırlama bağlantısı geçersiz.',
+  'user' => 'Bu e-posta adresine sahip bir kullanıcı bulunamadı.',
+ ],
+];
---
+++ resources/lang/tr/pagination.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+ 'previous' => 'pagination.previous',
+ 'next' => 'pagination.next',
+];
